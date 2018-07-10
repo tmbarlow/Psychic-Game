e9624db405cf5c9d6b708722dc2b833bf692545d
tmbarlow <tmbarlow@gmail.com>
2018-07-09 21:04:19 -0600

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,31 @@
 // computer guesses
 
-	var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-	var wins = 0;
-	var losses = 0;
-	var	guessLeft = 9;
-	var guessesSoFar = [];
-	var userGuess = null;
-	document.querySelector('#guessesLeft').innerHTML = guessLeft;
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var losses = 0;
+var	guessLeft = 9;
+var guessesSoFar = [];
+var userGuess = null;
+document.querySelector('#guessesLeft').innerHTML = guessLeft;
 
 	// computer makes a guess and it stores it to computerGuess
 	var computerGuess = options[Math.floor(Math.random()*options.length)];
 	console.log(computerGuess);
 
 //function to reset GuessesSoFar and guessLeft
-	function gameStart (){
+function gameStart (){
 	guessLeft = 9;
 	guessesSoFar = [];
 	computerGuess = computerGuess = options[Math.floor(Math.random()*options.length)];
-		console.log(computerGuess);
+	console.log(computerGuess);
 	document.querySelector('#guessSoFar').innerHTML = guessesSoFar;
 	document.querySelector('#guessesLeft').innerHTML = guessLeft;
-	}
-
-
+}
 
 //when user presses a alphapet key, it records it to UserGuess
 document.onkeyup = function(event){
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 	console.log(userGuess)
-
-
 
 	if (guessesSoFar.indexOf(userGuess) < 0 && options.indexOf(userGuess) >= 0) {
 		guessesSoFar[guessesSoFar.length]=userGuess;
@@ -43,10 +39,9 @@ document.onkeyup = function(event){
 		document.querySelector('#guessSoFar').innerHTML = guessesSoFar;
 	}
 
-
 	if (userGuess === computerGuess) {
-	wins++;
-	document.querySelector('#wins').innerHTML = wins;
+		wins++;
+		document.querySelector('#wins').innerHTML = wins;
 	//console.log(wins)
 	setTimeout(gameStart, 2000);
 	//console.log(guessLeft, guessesSoFar, computerGuess)
@@ -54,26 +49,21 @@ document.onkeyup = function(event){
 
 	else if (userGuess !== computerGuess && guessLeft > 0){
 	guessLeft--;
-	if (guessLeft == 0) {
-		setTimeout(gameStart, 2000);
+		if (guessLeft == 0) {
+			setTimeout(gameStart, 2000);
+		}
+		document.querySelector('#guessesLeft').innerHTML = guessLeft;
+		//console.log(guessLeft)
 	}
-	document.querySelector('#guessesLeft').innerHTML = guessLeft;
-	//console.log(guessLeft)
 
-	}
-
-	 if (userGuess !== computerGuess && guessLeft === 0) {
-	losses++;
-	document.querySelector('#losses').innerHTML = losses;
-	console.log(losses)
+	if (userGuess !== computerGuess && guessLeft === 0) {
+		losses++;
+		document.querySelector('#losses').innerHTML = losses;
+		console.log(losses)
 		setTimeout(gameStart, 2000);
-			if (losses === 3) {
-				alert("You're not very good at this, are you? Try again")
-				setTimeout(gameStart, 2000);
-
-			}
- 
- 	}
-
- 	
+		if (losses === 3) {
+			alert("You're not very good at this, are you? Try again")
+			setTimeout(gameStart, 2000);
+		}
+	}
 }
